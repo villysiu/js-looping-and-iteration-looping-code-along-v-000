f8e1bc23bb9b887e8f9a122ff396eb31ab7e5ad3
villysiu <villysiu@gmail.com>
2021-06-06 13:29:13 -0700

    commit tree e68d9620418a523b4b6ed838a765743147f3473c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(i) {
+    
+    while (i>=0) {
+        console.log(i--);
+    }
+  } 
+
+function writeCards(friendsArray, message) {
+    let i = 0;
+    const a = [];
+    while (i < friendsArray.length) {
+        a.push("Thank you, " + friendsArray[i] + ", for the wonderful " + (message) + " gift!");
+       i++;
+    }
+    return a;
+}
